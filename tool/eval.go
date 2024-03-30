5449a38c92b730d93ccfb8224632309f311c4435
Uchijo <kgtnttki.yuta@gmail.com>
2024-03-30 17:57:09 +0900

    preprocess [-]: 31.74s

--- tool/eval.go
+++ tool/eval.go
@@ -6,6 +6,7 @@ func Eval(src []uint8) {
 	mem := map[int]uint8{}
 	memPtr := 0
 	pc := 0
+	src = resetToZeroPattern(src)
 	jumpDest := cacheJumpDest(src)
 
 	for {
@@ -26,6 +27,8 @@ func Eval(src []uint8) {
 			fmt.Print(string(mem[memPtr]))
 		case ',':
 			// not implemented
+		case '0':
+			mem[memPtr] = 0
 		case '[':
 			if mem[memPtr] == 0 {
 				pc = jumpDest[pc]
@@ -38,6 +41,20 @@ func Eval(src []uint8) {
 
 		pc++
 	}
+}
+
+// find [-] pattern and replace it with 0
+func resetToZeroPattern(src []uint8) []uint8 {
+	retval := []uint8{}
+	for i := 0; i < len(src); i++ {
+		if i+2 < len(src) && src[i] == '[' && src[i+1] == '-' && src[i+2] == ']' {
+			retval = append(retval, '0')
+			i += 2
+		} else {
+			retval = append(retval, src[i])
+		}
+	}
+	return retval
 }
 
 func cacheJumpDest(src []uint8) map[int]int {
