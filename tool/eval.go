27bbc47ee0dc4bae0e16e2d3eec30fd167c4b520
Uchijo <kgtnttki.yuta@gmail.com>
2024-03-30 17:26:37 +0900

    calculate jump destination before evaluation: 33.59s

--- tool/eval.go
+++ tool/eval.go
@@ -6,6 +6,7 @@ func Eval(src []uint8) {
 	mem := map[int]uint8{}
 	memPtr := 0
 	pc := 0
+	jumpDest := cacheJumpDest(src)
 
 	for {
 		if pc >= len(src) {
@@ -27,38 +28,56 @@ func Eval(src []uint8) {
 			// not implemented
 		case '[':
 			if mem[memPtr] == 0 {
-				// find matching ']'
-				nest := 1
-				for {
-					pc++
-					if src[pc] == '[' {
-						nest++
-					} else if src[pc] == ']' {
-						nest--
-						if nest == 0 {
-							break
-						}
-					}
-				}
+				pc = jumpDest[pc]
 			}
 		case ']':
 			if mem[memPtr] != 0 {
-				// find matching '['
-				nest := 1
-				for {
-					pc--
-					if src[pc] == ']' {
-						nest++
-					} else if src[pc] == '[' {
-						nest--
-						if nest == 0 {
-							break
-						}
-					}
-				}
+				pc = jumpDest[pc]
 			}
 		}
 
 		pc++
 	}
+}
+
+func cacheJumpDest(src []uint8) map[int]int {
+	jumpDest := map[int]int{}
+	for pc, c := range src {
+		if c == '[' {
+			start := pc
+			nest := 1
+			for {
+				start++
+				if src[start] == '[' {
+					nest++
+				} else if src[start] == ']' {
+					nest--
+					if nest == 0 {
+						break
+					}
+				} else {
+					continue
+				}
+			}
+			jumpDest[pc] = start
+		} else if c == ']' {
+			start := pc
+			nest := 1
+			for {
+				start--
+				if src[start] == ']' {
+					nest++
+				} else if src[start] == '[' {
+					nest--
+					if nest == 0 {
+						break
+					}
+				} else {
+					continue
+				}
+			}
+			jumpDest[pc] = start
+		}
+	}
+	return jumpDest
 }
