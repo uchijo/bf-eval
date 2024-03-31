c891d4f03528d724e203581e03cd993556144d3d
Uchijo <kgtnttki.yuta@gmail.com>
2024-03-31 20:57:56 +0900

    shrink memory size

--- tool/mem_store.go
+++ tool/mem_store.go
@@ -3,21 +3,21 @@ package tool
 type MemStore []uint8
 
 func (m MemStore) Get(ptr int) uint8 {
-	return m[ptr + 30000]
+	return m[ptr+2048]
 }
 
 func (m MemStore) Set(ptr int, v uint8) {
-	m[ptr + 30000] = v
+	m[ptr+2048] = v
 }
 
 func (m MemStore) AddTo(ptr int, v uint8) {
-	m[ptr + 30000] += v
+	m[ptr+2048] += v
 }
 
 func (m MemStore) SubFrom(ptr int, v uint8) {
-	m[ptr + 30000] -= v
+	m[ptr+2048] -= v
 }
 
 func NewMemStore() MemStore {
-	return make(MemStore, 60000)
+	return make(MemStore, 4096)
 }
